feea0b01af2d50cdc74e699bed37d6db7b09b1ee
Sbax <heysbax@protonmail.com>
2022-01-27 17:02:20 +0100

    Fixed for ugly phones

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,10 +8,12 @@ export const Container = styled.main`
 
   height: 100%;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  @media (min-width: ${breakpoints.tablet}) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
   > * + * {
     margin-top: 1rem;
